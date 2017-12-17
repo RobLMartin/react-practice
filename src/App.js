b5b9f03dbd55eb5059b46cceeea8f5f0b1927332
Rob Martin <rmartin3@alumni.unca.edu>
2017-12-17 13:41:10 -0500

    Component vs React.createElement and custom components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello, im a react app!</h1>
+        <p>This is really working.</p>
+        <Person />
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'this text'));
   }
 }
 
