371fa0ba02592c7ab4650d03a08dfe42e1d37797
Rob Martin <rmartin3@alumni.unca.edu>
2017-12-17 15:07:38 -0500

    understanding properties in react.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hello, im a react app!</h1>
         <p>This is really working.</p>
-        <Person />
+        <Person name='Rob' age='27'/>
+        <Person  name='Brett' age='26'>I'm an artist!</Person>
+        <Person  name='Max' age='28'/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'this text'));
