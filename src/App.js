f8a1b2a67e0bcf2716dca038863c74987ed013e9
Rob Martin <rmartin3@alumni.unca.edu>
2017-12-17 15:57:38 -0500

    added two-way binding

--- src/App.js
+++ src/App.js
@@ -23,6 +23,16 @@ class App extends Component {
      });
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 27 },
+        { name: 'Brett', age: 26 }
+      ]
+     });
+  }
+
   render() {
     return (
       <div className="App">
@@ -35,7 +45,8 @@ class App extends Component {
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Leonhardt')}>I'm an artist!</Person>
+          click={this.switchNameHandler.bind(this, 'Leonhardt')}
+          changed={this.nameChangedHandler}>I'm an artist!</Person>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age}/>
