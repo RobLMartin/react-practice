7157901875dd802325b5cb3f77ccd570e340b3c8
Rob Martin <rmartin3@alumni.unca.edu>
2017-12-18 19:05:23 -0500

    error handling in react.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,14 @@ import React from 'react';
 import styles from './Person.css';
 
 const person  = ( props ) => {
+    const rnd = Math.random();
+
+    if (rnd > 0.7) {
+        throw new Error('Something went wrong.');
+    }
 
     return (
+        
         <div className={styles.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
